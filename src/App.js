1790d21a77126a18a191c06ec6a5c05abd9d6724
marwa <105356245+jafmarwa@users.noreply.github.com>
2023-02-02 16:53:07 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+       <h1>welcome to check-point CLOUD</h1>
+      <div>
+      <iframe title="This is a unique title" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12951.334056680334!2d10.819001126647532!3d35.754896596335335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x130212c171d486eb%3A0xc2a506c8ffdc957b!2sCit%C3%A9%20El%20Agba%2C%20Monastir!5e0!3m2!1sfr!2stn!4v1675352802790!5m2!1sfr!2stn" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </div>
+  );
+}
+
+export default App;
